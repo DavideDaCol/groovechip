aa5d3c40555e0d21c8f6f22b97f46786ecc58923
MZ04 <marco.zanatta@studenti.unitn.it>
2025-11-26 14:39:54 +0100

    added src directory

---
+++ src/main/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_mac.h"
+#include "driver/gpio.h"
+
+#define BLINK_GPIO 2
+
+void simpleTask(void *parameters){
+    int counter = 0;
+    while(true){
+        printf("esp32 is working, second %i\n", counter);
+        counter += 1;
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+void app_main(void)
+{
+    xTaskCreate(&simpleTask, "simple task", 2048, NULL, 5, NULL);
+}
