9bb442ddec173d1fb4b41198bfc75eb5af2098ac
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2025-12-01 21:43:46 +0100

    add dummy wav test

--- components/i2s/i2s_driver.c
+++ components/i2s/i2s_driver.c
@@ -7,49 +7,93 @@
 #include "driver/gpio.h"
 #include "esp_check.h"
 #include "sdkconfig.h"
+#include "dummy_wav.h"
 
-#define BUFF_SIZE 2048
+#define BUFF_SIZE 256
 
-static i2s_chan_handle_t out_chan;
+static i2s_chan_handle_t out_chan; // master channel for the output
+unsigned const char* wav_data_ptr; // points to the pure wav file
+uint32_t wav_data_idx = 0; //keeps track of how far ahead the playback is
+
+// a WAV file is made of a header with info + pure audio samples
+// this struct holds that information to ensure everything is set up
+// correctly and garbage isn't being loaded into the esp32
+
+struct wav_header_t
+    {
+      //   RIFF Section    
+      char riff_section_id[4];      // literally the letters "RIFF"
+      uint32_t size;                // (size of entire file) - 8
+      char riff_format[4];          // literally "WAVE" (for our usecase)
+      
+      //   Format Section    
+      char format_id[4];            // letters "fmt"
+      uint32_t format_size;         // (size of format section) - 8
+      uint16_t fmt_id;              // 1=uncompressed PCM, any other number isn't WAV
+      uint16_t num_channels;        // 1=mono,2=stereo
+      uint32_t sample_rate;         // 44100, 16000, 8000 etc.
+      uint32_t byte_rate;           // sample_rate * channels * (bits_per_sample/8)
+      uint16_t block_align;         // channels * (bits_per_sample/8)
+      uint16_t bits_per_sample;     // 8,16,24 or 32
+    
+      // Data Section
+      char data_id[4];              // literally the letters "data"
+      uint32_t data_size;           // Size of the data that follows
+    }WavHeader;
+
 
 static void i2s_example_write_task(void *args)
 {
-    uint8_t *w_buf = (uint8_t *)calloc(1, BUFF_SIZE);
-    assert(w_buf); // Check if w_buf allocation success
+    //size of the WAV header, it's always the same size in every WAV file
+    const uint32_t header_size = 44;
 
-    /* fills the buffer with fake data */
-    for (int i = 0; i < BUFF_SIZE; i += 8) {
-        w_buf[i]     = 0x12;
-        w_buf[i + 1] = 0x34;
-        w_buf[i + 2] = 0x56;
-        w_buf[i + 3] = 0x78;
-        w_buf[i + 4] = 0x9A;
-        w_buf[i + 5] = 0xBC;
-        w_buf[i + 6] = 0xDE;
-        w_buf[i + 7] = 0xF0;
-    }
+    //copy the WAV header into the struct so we can analyze it
+    memcpy(&WavHeader, WavData, header_size);
+
+    //set the WAV pointer to start at the beginning of the audio samples
+    wav_data_ptr = WavData + header_size;
 
     size_t w_bytes = BUFF_SIZE;
 
-    /* Preloads the channel with data, this is only for test purposes */
-    while (w_bytes == BUFF_SIZE) {
-        /* load the target buffer repeatedly, until all the DMA buffers are preloaded */
-        ESP_ERROR_CHECK(i2s_channel_preload_data(out_chan, w_buf, BUFF_SIZE, &w_bytes));
-    }
+    // create a i2s DMA buffer to write samples without stressing the CPU.
+    // Has to be double the chosen size to allow stereo playback
+    int16_t *i2s_buf = malloc(BUFF_SIZE * 2 * sizeof(int16_t));
+    assert(i2s_buf);
 
-    /* Enable the output channel */
     ESP_ERROR_CHECK(i2s_channel_enable(out_chan));
-    while (1) {
-        /* Write i2s data */
-        if (i2s_channel_write(out_chan, w_buf, BUFF_SIZE, &w_bytes, portMAX_DELAY) == ESP_OK) {
-            printf("Write Task: i2s write %d bytes\n", w_bytes); // this print is just for testing, blocking I/O should be avoided at all cost
-        } else {
-            printf("Write Task: i2s write failed\n");
+
+    // quick and dirty volume control. 
+    // WARNING: the stereo out is usually very loud, this value shoud stay below 0.1
+    float volume = 0.05f; 
+
+     while (1) {
+        for (int i = 0; i < BUFF_SIZE; i++) {
+
+            // in WAV files, left and right samples are sequential
+            int16_t left  = *(int16_t*)(wav_data_ptr + wav_data_idx);
+            int16_t right = *(int16_t*)(wav_data_ptr + wav_data_idx + 2);
+
+            left  *= volume;
+            right *= volume;
+
+            // writes the WAV data to the buffer post volume adjustment
+            i2s_buf[i * 2] = left;
+            i2s_buf[i * 2 + 1] = right;
+
+            wav_data_idx += 4;
+
+            // loop the audio file
+            if (wav_data_idx >= WavHeader.data_size)
+                wav_data_idx = 0;
         }
-        //tells the rest of the program it has 10 ms to do whatever it wants before the task starts reading the i2s buffer again
-        vTaskDelay(pdMS_TO_TICKS(10));
+
+        // Write full buffer (1024 bytes)
+        ESP_ERROR_CHECK(i2s_channel_write(out_chan, i2s_buf,
+                                          BUFF_SIZE * 2 * sizeof(int16_t),
+                                          &w_bytes,
+                                          portMAX_DELAY));
     }
-    free(w_buf);
+    free(i2s_buf);
     vTaskDelete(NULL);
 }
 
@@ -58,8 +102,8 @@ void i2s_driver_init(){
     ESP_ERROR_CHECK(i2s_new_channel(&out_chan_cfg, &out_chan, NULL));
 
     i2s_std_config_t out_port_cfg = {
-        .clk_cfg  = I2S_STD_CLK_DEFAULT_CONFIG(16000), //clock works at 16kHz
-        .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO), // output data resolution is 32 bits per sample in stereo
+        .clk_cfg  = I2S_STD_CLK_DEFAULT_CONFIG(16000), //ideally should match the WAV files
+        .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO), // data resolution is 16 bits per sample, stereo
         .gpio_cfg = {
             .mclk = I2S_GPIO_UNUSED,    // some codecs may require a master clock signal
             .bclk = GRVCHP_OUT_BCLK,
