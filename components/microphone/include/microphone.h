378ee7e5a2ee879330e9e6aa789b61f32ef79ee6
MZ04 <marco.zanatta@studenti.unitn.it>
2026-01-29 14:54:32 +0100

    removed microphone folder

--- components/microphone/include/microphone.h
+++
@@ -1,9 +0,0 @@
-#ifndef MIC_H
-#define MIC_H
-
-#include "i2s_driver.h"
-#include "mixer.h"
-#include <stdint.h>
-
-sample_t record_sample(void);
-#endif
--- components/microphone/microphone.c
+++
@@ -1,5 +0,0 @@
-#include "microphone.h"
-
-sample_t record_sample(i2s_chan_handle_t channel) {
-    
-}
