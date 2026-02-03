3c0c90c4e020739a2dfba1e32595b47784074bda
MZ04 <marco.zanatta@studenti.unitn.it>
2026-02-03 12:04:23 +0100

    first mic channel config implementation

--- components/i2s/i2s_driver.c
+++ components/i2s/i2s_driver.c
@@ -8,7 +8,7 @@
 #include "esp_check.h"
 #include "sdkconfig.h"
 
-i2s_chan_handle_t i2s_driver_init(){
+i2s_chan_handle_t i2s_mixer_init(){
 
     i2s_chan_handle_t out_channel; // master channel for the output
 
@@ -20,10 +20,10 @@ i2s_chan_handle_t i2s_driver_init(){
         .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO), // data resolution is 16 bits per sample, stereo
         .gpio_cfg = {
             .mclk = I2S_GPIO_UNUSED,    // some codecs may require a master clock signal
-            .bclk = GRVCHP_OUT_BCLK,
+            .bclk = GRVCHP_OUT_MIX_BCLK,
             .ws   = GRVCHP_OUT_WS,
             .dout = GRVCHP_OUT_DOUT,
-            .din  = GRVCHP_OUT_DIN,
+            .din  = GRVCHP_OUT_DIN,    //TODO: if used, check whether it's right to use the same connection for both the mixer and the mic
             .invert_flags = {
                 .mclk_inv = false,
                 .bclk_inv = false,
@@ -34,6 +34,36 @@ i2s_chan_handle_t i2s_driver_init(){
 
     //init the i2s channel with the given configs
     ESP_ERROR_CHECK(i2s_channel_init_std_mode(out_channel, &out_port_cfg));
+
+    return out_channel;
+}
+
+i2s_chan_handle_t i2s_microphone_init(){
+
+    i2s_chan_handle_t in_channel; 
+
+    i2s_chan_config_t in_chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
+    ESP_ERROR_CHECK(i2s_new_channel(&in_chan_cfg, &in_channel, NULL));
+
+    i2s_std_config_t in_port_cfg = {
+        .clk_cfg  = I2S_STD_CLK_DEFAULT_CONFIG(15000), //Frequency based on the INMP441 max parameter
+        .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO), //Default parameters
+        .gpio_cfg = {
+            .mclk = I2S_GPIO_UNUSED,    
+            .bclk = GRVCHP_OUT_MIC_BCLK,
+            .ws   = GRVCHP_OUT_WS,
+            .dout = I2S_GPIO_UNUSED,
+            .din  = GRVCHP_OUT_DIN,
+            .invert_flags = {
+                .mclk_inv = false,
+                .bclk_inv = false,
+                .ws_inv   = false,
+            },
+        },
+    };
+
+    //init the i2s channel with the given configs
+    ESP_ERROR_CHECK(i2s_channel_init_std_mode(in_channel, &in_port_cfg));
 
     return out_channel;
 }
--- components/i2s/include/i2s_driver.h
+++ components/i2s/include/i2s_driver.h
@@ -4,11 +4,12 @@
 #include <stdint.h>
 #include "driver/i2s_types.h"
 
-#define GRVCHP_OUT_BCLK 18
+#define GRVCHP_OUT_MIX_BCLK 18
 #define GRVCHP_OUT_WS 15
 #define GRVCHP_OUT_DOUT 22
 #define GRVCHP_OUT_DIN 23
 
-i2s_chan_handle_t i2s_driver_init();
+i2s_chan_handle_t i2s_mixer_init();
+i2s_chan_handle_t i2s_microphone_init();
 
 #endif
